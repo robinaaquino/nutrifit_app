97000e209afe50cc9a594e3a5f875a72cedb505e
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-08 23:51:41 +0800

    chore: testing netlify variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import React, { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+
+    if(process.env.TEST_KEY === `uwu`) {
+      console.log('This is a test')
+    }
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">
