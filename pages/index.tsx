9e069a250895351ad6eabbdfa31920d1119a9eb9
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-14 21:30:54 +0800

    chore: sample firebase function

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { getAllUsers } from '../firebase/services/users_services'
 import React, { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   useEffect(() => {
+    getAllUsers().then(
+      result => {
+        console.log(result)
+      }
+    )
 
     if(process.env.TEST_KEY === `uwu`) {
       console.log('This is a test')
