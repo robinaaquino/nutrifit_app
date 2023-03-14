685971cd2f0907cb4e60d67e0b8c807e0e88b76c
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-03-14 21:30:29 +0800

    style: added custom colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      nf_green: '#00A858',
+      nf_yellow: '#FFF011',
+      nf_dark_blue: '#326273',
+      nf_light_blue: '#5C9EAD'
+    },
     extend: {},
   },
   plugins: [],
