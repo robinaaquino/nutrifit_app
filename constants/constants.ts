72a4518c666fa0ba0fecadd77bcdde442def1564
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-10 18:34:36 +0800

    feat: added interface for function results for backend

--- constants/constants.ts
+++ constants/constants.ts
@@ -2,3 +2,10 @@ export const NOTIFICATION_STATES = {
   SUCCESS: "success",
   ERROR: "error",
 };
+
+export interface functionResult {
+  result: any;
+  isSuccess: Boolean;
+  resultText: String;
+  errorMessage: String;
+}
