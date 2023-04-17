e867e2d01f33bd00842b2d93c67fcb3d8036e69d
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-17 15:15:01 +0800

    feat: navigated user to maintenance page when navigating to individual products

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router";
+import React, { useEffect } from "react";
 export default function ProductShow() {
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(() => {
+    router.replace("/maintenance");
+  });
   return <>{id}</>;
 }
