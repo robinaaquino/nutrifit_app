3f04a6352a6033bda18ff76348bdd28f03b1e30e
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-24 13:57:15 +0800

    feat: changed method to upload image

--- pages/admin/product/add.tsx
+++ pages/admin/product/add.tsx
@@ -1,8 +1,7 @@
 import { useForm } from "react-hook-form";
 import { useContext, useState } from "react";
 import Image from "next/image";
-// import no_image from "../../public/no_image.png";
-import no_image from "public/no_image.png";
+import no_image from "../../../public/no_image.png";
 import {
   PRODUCT_CATEGORIES_ARRAY,
   PRODUCT_CATEGORIES_PUBLIC_NAME_ARRAY,
@@ -23,7 +22,7 @@ export default function AdminAddProduct(props: any) {
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(0);
   const [productDescription, setProductDescription] = useState("");
-  const [files, setFile] = useState<any>([]);
+  const [files, setFiles] = useState<any>([]);
   const [message, setMessage] = useState("");
   const productImagesArray = [0, 1, 2, 3];
 
@@ -53,41 +52,29 @@ export default function AdminAddProduct(props: any) {
       }
     };
 
-  const handleFile = (e: any) => {
+  const handleIndividualFile = (e: any, index: any) => {
     setMessage("");
     let file = e.target.files;
+    let previousFiles = [...files];
 
-    for (let i = 0; i < file.length; i++) {
-      const fileType = file[i]["type"];
+    if (file[0].name) {
+      const fileType = file[0]["type"];
       const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
       if (validImageTypes.includes(fileType)) {
-        setFile([...files, file[i]]);
+        previousFiles[index] = file[0];
       } else {
-        setMessage("only images accepted");
+        authContextObject.error("Only images accepted");
       }
+    } else {
+      previousFiles[index] = file[0];
     }
+    setFiles(previousFiles);
   };
-  // const handleIndividualFile = (e: any, place: number) => {
-  //   setMessage("");
 
-  //   let file = e.target.file;
-
-  //   const fileType = file["type"];
-  //   const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
-
-  //   if (validImageTypes.includes(fileType)) {
-  //     let previousArray = [...files];
-  //     let item = previousArray[place];
-  //     item = file;
-  //     previousArray[place] = item;
-  //     setFile(previousArray);
-  //   } else {
-  //     setMessage("only images accepted");
-  //   }
-  // };
-
-  const removeImage = (i: any) => {
-    setFile(files.filter((x: any) => x.name !== i));
+  const removeIndividualImage = (i: any) => {
+    let previousFiles = [...files];
+    previousFiles[i] = "";
+    setFiles(previousFiles);
   };
 
   if (props.isError) {
@@ -242,74 +229,89 @@ export default function AdminAddProduct(props: any) {
           <div>
             <div className="h-full">
               <div>Product Images</div>
-              {/* {productImagesArray.map((key: any) => {
-                console.log(files[key]);
-                return (
-                  <>
-                    <input
-                      type="file"
-                      onChange={(e: any) => handleIndividualFile(e, key)}
-                      className="h-full w-full bg-nf_green opacity-0 z-10 absolute"
-                      multiple
-                      name="file[]"
-                    >
-                      ( files[key] ?
-                      <img
-                        className="h-20 w-20 rounded-md"
-                        src={URL.createObjectURL(files[key])}
-                      />
-                      {files[key]}
-                      :
-                      <Image
-                        className="m-auto"
-                        src={no_image}
-                        alt="no image"
-                        width="256"
-                        height="256"
-                      />
-                      )
-                    </input>
-                  </>
-                );
-              })} */}
-              <div className="h-screen flex justify-center items-center bg-gray-300 px-2">
-                <div className="p-3 md:w-1/2 w-[360px] bg-white rounded-md">
-                  <span className="flex justify-center items-center text-[12px] mb-1 text-red-500">
-                    {message}
-                  </span>
-                  <div className="h-32 w-full relative border-2 items-center rounded-md cursor-pointer bg-gray-300 border-gray-400 border-dotted">
-                    <input
-                      type="file"
-                      onChange={handleFile}
-                      className="h-full w-full bg-green-200 opacity-0 z-10 absolute"
-                      multiple
-                      name="files[]"
-                    />
-                    <div className="h-full w-full bg-gray-200 absolute z-1 flex justify-center items-center top-0">
-                      <div className="flex flex-col">
-                        <i className="mdi mdi-folder-open text-[30px] text-gray-400 text-center"></i>
-                        <span className="text-[12px]">{`Drag and Drop a file`}</span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="flex flex-wrap gap-2 mt-2">
-                    {files.map((file: any, key: any) => {
-                      return (
-                        <div key={key} className="overflow-hidden relative">
-                          <i
-                            onClick={() => {
-                              removeImage(file.name);
-                            }}
-                            className="mdi mdi-close absolute right-1 hover:text-white cursor-pointer"
-                          ></i>
-                          <img
-                            className="h-20 w-20 rounded-md"
-                            src={URL.createObjectURL(file)}
-                          />
-                        </div>
-                      );
-                    })}
-                  </div>
+
+              <div className="flex flex-col w-auto min-h-screen p-10 bg-gray-100 text-gray-800">
+                <div className="grid  grid-cols-2 gap-x-6 gap-y-12 w-full mt-6">
+                  {/* Product Tile Start */}
+                  {files != undefined
+                    ? productImagesArray.map((e) => {
+                        var fileImage: any = no_image;
+                        files[e] === undefined
+                          ? (fileImage = no_image)
+                          : files[e].name != undefined
+                          ? (fileImage = URL.createObjectURL(files[e]))
+                          : (fileImage = files[e]);
+                        return (
+                          <>
+                            <div
+                              key={e}
+                              className="overflow-hidden relative bg-gray-500"
+                            >
+                              {files[e] === undefined || files[e] === "" ? (
+                                <div className="h-96 w-96  border-2 items-center rounded-md  bg-gray-300 border-gray-400 border-dotted ">
+                                  <input
+                                    type="file"
+                                    onChange={(event) =>
+                                      handleIndividualFile(event, e)
+                                    }
+                                    className="w-full h-full bg-green-200 opacity-0 z-10 absolute cursor-pointer"
+                                    name="files[]"
+                                  />
+                                  <div className="h-full w-full bg-gray-200 absolute z-1 flex justify-center items-center top-0 ">
+                                    <div className="flex flex-col text-center m-auto mt-2">
+                                      <span className="text-[12px] mb-2">{`Upload an image`}</span>
+
+                                      <Image
+                                        className="m-auto object-cover "
+                                        src={no_image}
+                                        alt="no_image"
+                                        width="1024"
+                                        height="1024"
+                                      />
+                                    </div>
+                                  </div>
+                                  <p>where the fuck</p>
+                                </div>
+                              ) : (
+                                <div className="justify-center items-center">
+                                  <div className="flex cursor-pointer   bg-white  w-auto justify-center">
+                                    <button
+                                      onClick={() => {
+                                        removeIndividualImage(e);
+                                      }}
+                                      className="flex rounded-full bg-white text-black "
+                                    >
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+                                        className="bi bi-x-circle m-auto"
+                                        viewBox="0 0 16 16"
+                                      >
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                      </svg>
+                                      <span className="px-2">Remove</span>
+                                    </button>
+                                  </div>
+
+                                  {/* TODO:doesnt show image when it showsin catalog */}
+                                  <Image
+                                    className="m-auto object-cover"
+                                    src={fileImage}
+                                    alt={fileImage}
+                                    width="1024"
+                                    height="1024"
+                                  />
+                                  {/* <p>{fileImage}</p> */}
+                                </div>
+                              )}
+                            </div>
+                          </>
+                        );
+                      })
+                    : null}
                 </div>
               </div>
             </div>
@@ -322,83 +324,6 @@ export default function AdminAddProduct(props: any) {
             </div>
           </div>
         </div>
-
-        {/* <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-password"
-            >
-              Password
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="password"
-              placeholder="******************"
-            />
-            <p className="text-gray-600 text-xs italic">
-              Make it as long and as crazy as you'd like
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-city"
-            >
-              City
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-city"
-              type="text"
-              placeholder="Albuquerque"
-            />
-          </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-state"
-            >
-              State
-            </label>
-            <div className="relative">
-              <select
-                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-state"
-              >
-                <option>New Mexico</option>
-                <option>Missouri</option>
-                <option>Texas</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-zip"
-            >
-              Zip
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-zip"
-              type="text"
-              placeholder="90210"
-            />
-          </div>
-        </div> */}
       </form>
     </>
   );
@@ -406,7 +331,6 @@ export default function AdminAddProduct(props: any) {
 
 export async function getServerSideProps(context: any) {
   try {
-    console.log("server side auth be like");
     const cookies = nookies.get(context);
     const token = await admin.auth().verifyIdToken(cookies.token);
 
