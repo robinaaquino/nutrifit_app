201a1a38368d6bf233f6e9cfde663db709f1699b
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-25 00:25:03 +0800

    feat: implemented cart functions in context

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -12,6 +12,11 @@ import nookies from "nookies";
 
 import { useState } from "react";
 import { isUserAuthorizedFunction } from "@/firebase/firebase_functions/users_function";
+import {
+  getCartViaIdFunction,
+  addToCartFunction,
+  removeFromCartFunction,
+} from "@/firebase/firebase_functions/cart_function";
 
 const auth = getAuth(app);
 
@@ -26,6 +31,9 @@ export const AuthContext = createContext({
   clear: () => {},
   reset: () => {},
   setUserAndAuthorization: (id: string, authorized: boolean) => {},
+  cart: [],
+  addToCart: (product: any, quantity: any) => {},
+  removeFromCart: (product: any) => {},
 });
 
 export const useAuthContext = () => useContext(AuthContext);
@@ -36,6 +44,7 @@ export const AuthContextProvider = ({ children }: { children?: ReactNode }) => {
   const [isAuthorized, setAuthorized] = useState<boolean>(false);
   const [notification, setNotification] = useState<string>("");
   const [notificationText, setNotificationText] = useState<string>("");
+  const [cart, setCart] = useState<any>(null);
 
   const success = (text: string) => {
     window.scroll(0, 0);
@@ -65,6 +74,298 @@ export const AuthContextProvider = ({ children }: { children?: ReactNode }) => {
   const setUserAndAuthorization = (id: string, authorized: boolean) => {
     setUser(id);
     setAuthorized(authorized);
+  };
+
+  const isProductInArray = (product: any, products: any) => {
+    for (let i = 0; i < products.length; i++) {
+      if (product.id == products[i].id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const addProductToContextCart = (product: any, quantity: any) => {
+    console.log("Adding product to context cart");
+    console.log("Before: ", cart);
+    const productToBeAddedToCart = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      category: product.category,
+      quantity: quantity,
+    };
+
+    if (cart == null) {
+      const cartObject = {
+        created_at: new Date().toString(),
+        products: [productToBeAddedToCart],
+        subtotal_price: product.price * quantity,
+        updated_at: new Date().toString(),
+        user: user ? user : null,
+      };
+      setCart(cartObject);
+    } else {
+      var cartObject = cart;
+
+      if (isProductInArray(product, cartObject.products)) {
+        return false;
+      } else {
+        cartObject.products = [...cartObject.products, productToBeAddedToCart];
+        cartObject.subtotal_price =
+          cartObject.subtotal_price + product.price * quantity;
+        cartObject.updated_at = new Date().toString();
+        setCart(cartObject);
+        console.log("After: ", cart);
+        return true;
+      }
+    }
+  };
+
+  const removeProductFromContextCart = (product: any) => {
+    if (cart.length == 0) {
+      console.log("Removing product from context cart");
+      console.log("Before: ", cart);
+
+      var cartObject = cart;
+      let previousProductsInCart = cartObject.products;
+
+      for (let i = 0; i < cartObject.products.length; i++) {
+        if (product.id == cartObject.products[i].id) {
+          previousProductsInCart.splice(i, 1);
+          break;
+        }
+      }
+      cartObject.products = previousProductsInCart;
+
+      cartObject.subtotal_price =
+        cartObject.subtotal_price - product.price * product.quantity;
+      cartObject.updated_at = new Date().toString();
+
+      console.log("After: ", cartObject);
+      setCart(cartObject);
+    }
+    return true;
+  };
+
+  const addProductToCookiesCart = (product: any, quantity: any) => {
+    const productToBeAddedToCart = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      category: product.category,
+      quantity: quantity,
+    };
+
+    const cookies = nookies.get(undefined);
+
+    if (cookies.cart != undefined) {
+      var cartObjectFromCookies = JSON.parse(cookies.cart);
+
+      if (isProductInArray(product, cartObjectFromCookies.products)) {
+        return false;
+      } else {
+        let previousProducts = cartObjectFromCookies.products;
+        previousProducts.push(productToBeAddedToCart);
+        cartObjectFromCookies.subtotal_price =
+          cartObjectFromCookies.subtotal_price + product.price * quantity;
+        cartObjectFromCookies.updated_at = new Date().toString();
+
+        nookies.set(undefined, "cart", JSON.stringify(cartObjectFromCookies), {
+          path: "/",
+        });
+        return true;
+      }
+    } else {
+      //initialize cart in cookies
+      const cartObject = {
+        created_at: new Date().toString(),
+        products: [productToBeAddedToCart],
+        subtotal_price: product.price * quantity,
+        updated_at: new Date().toString(),
+        user: user ? user : null,
+      };
+
+      nookies.set(undefined, "cart", JSON.stringify(cartObject), {
+        path: "/",
+      });
+      return true;
+    }
+  };
+
+  const removeProductFromCookiesCart = (product: any) => {
+    const cookies = nookies.get(undefined);
+
+    if (cookies.cart != undefined) {
+      var cartObjectFromCookies = JSON.parse(cookies.cart);
+
+      let previousProductsInCart = cartObjectFromCookies.products;
+
+      for (let i = 0; i < cartObjectFromCookies.products.length; i++) {
+        if (product.id == cartObjectFromCookies.products[i].id) {
+          previousProductsInCart.splice(i, 1);
+          break;
+        }
+      }
+      cartObjectFromCookies.products = previousProductsInCart;
+
+      cartObjectFromCookies.subtotal_price =
+        cartObjectFromCookies.subtotal_price - product.price * product.quantity;
+      cartObjectFromCookies.updated_at = new Date().toString();
+
+      nookies.set(undefined, "cart", JSON.stringify(cartObjectFromCookies), {
+        path: "/",
+      });
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const addToCart = async (product: any, quantity: any, user?: any) => {
+    const productToBeAddedToCart = {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      category: product.category,
+      quantity: quantity,
+    };
+
+    // if (cart == null) {
+    //   //check cookies, if there's cart in cookies
+    //   const cookies = nookies.get(undefined);
+    //   console.log("cookies:", cookies);
+
+    //   if (cookies.cart != undefined) {
+    //     //if cart in cookies exists, get information and update context and cookies
+    //     var cartObject = JSON.parse(cookies.cart);
+    //     cartObject.products = [...cartObject.products, productToBeAddedToCart];
+    //     cartObject.subtotal_price =
+    //       cartObject.subtotal_price + product.price * quantity;
+    //     cartObject.updated_at = new Date().toString();
+
+    //     setCart(cartObject);
+    //     nookies.set(undefined, "cart", JSON.stringify(cartObject), {
+    //       path: "/",
+    //     });
+    //     success("Success in adding product to cart");
+    //   } else if (user) {
+    //     //get cart from firebase if there's no context cart and cookies cart
+    //     const getCartResult = await getCartViaIdFunction(user);
+
+    //     if (getCartResult.isSuccess) {
+    //       const addCartResult = await addToCartFunction(
+    //         product,
+    //         quantity,
+    //         user
+    //       );
+
+    //       if (addCartResult.isSuccess) {
+    //         setCart(addCartResult.result);
+    //         nookies.set(
+    //           undefined,
+    //           "cart",
+    //           JSON.stringify(addCartResult.result),
+    //           {
+    //             path: "/",
+    //           }
+    //         );
+    //         success("Success in adding product to cart");
+    //       } else {
+    //         error("Error with adding product to cart");
+    //       }
+    //     } else {
+    //       error("Error with getting cart results");
+    //     }
+    //   } else {
+    //     //initialize cart and update context and cookies
+    //     const cartObject = {
+    //       created_at: new Date().toString(),
+    //       products: [productToBeAddedToCart],
+    //       subtotal_price: product.price * quantity,
+    //       updated_at: new Date().toString(),
+    //       user: user ? user : null,
+    //     };
+
+    //     setCart(cartObject);
+    //     nookies.set(undefined, "cart", JSON.stringify(cartObject), {
+    //       path: "/",
+    //     });
+    //     success("Success in adding product to cart");
+    //   }
+    // } else {
+    //   //get cart from context and update cookies
+    //   var cartObject = cart;
+    //   cartObject.products = [...cartObject.products, productToBeAddedToCart];
+    //   cartObject.subtotal_price =
+    //     cartObject.subtotal_price + product.price * quantity;
+    //   cartObject.updated_at = new Date().toString();
+    //   setCart(cartObject);
+    //   nookies.set(undefined, "cart", JSON.stringify(cartObject), {
+    //     path: "/",
+    //   });
+
+    //   success("Success in adding product to cart");
+    // }
+
+    if (user) {
+      const addCartResult = await addToCartFunction(product, quantity, user);
+
+      if (addCartResult.isSuccess) {
+        const contextResult = addProductToContextCart(
+          productToBeAddedToCart,
+          quantity
+        );
+        const cookieResult = addProductToCookiesCart(
+          productToBeAddedToCart,
+          quantity
+        );
+
+        if (contextResult == false || cookieResult == false) {
+          error("Duplicate product in cart");
+        } else {
+          success("Successful in adding product to cart");
+        }
+      } else {
+        error(addCartResult.errorMessage);
+      }
+    } else {
+      const contextResult = addProductToContextCart(
+        productToBeAddedToCart,
+        quantity
+      );
+      const cookieResult = addProductToCookiesCart(
+        productToBeAddedToCart,
+        quantity
+      );
+
+      if (contextResult == false || cookieResult == false) {
+        error("Duplicate product in cart");
+      } else {
+        success("Sucessful in adding product to cart");
+      }
+    }
+  };
+
+  const removeFromCart = async (product: any) => {
+    if (user) {
+      const removeFromCartResult = await removeFromCartFunction(product, user);
+
+      if (removeFromCartResult.isSuccess) {
+        removeProductFromContextCart(product);
+        removeProductFromCookiesCart(product);
+        success("Sucessful in removing product from cart");
+      } else {
+        error(removeFromCartResult.errorMessage);
+      }
+    } else {
+      removeProductFromContextCart(product);
+      removeProductFromCookiesCart(product);
+      success("Sucessful in removing product from cart");
+    }
   };
 
   useEffect(() => {
@@ -111,6 +412,9 @@ export const AuthContextProvider = ({ children }: { children?: ReactNode }) => {
         notificationText,
         reset,
         setUserAndAuthorization,
+        cart,
+        addToCart,
+        removeFromCart,
       }}
     >
       {loading ? <div>Loading...</div> : children}
@@ -120,5 +424,7 @@ export const AuthContextProvider = ({ children }: { children?: ReactNode }) => {
 
 export const logout = async () => {
   const auth = getAuth();
+  nookies.destroy(undefined, "token");
+  nookies.destroy(undefined, "cart");
   await signOut(auth);
 };
