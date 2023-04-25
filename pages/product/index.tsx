38999036e0d302d89f42d9ba1b44e1f77ccb5aa7
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-26 00:24:03 +0800

    feat: implemented sort with limited options for product catalog

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -85,6 +85,32 @@ export default function Catalog(props: any) {
     }
   }
 
+  const sortBy = (text: string, order: string) => {
+    var key = "quantity_sold";
+    if (text == "popular") {
+      key = "quantity_sold";
+    } else if (text == "newest") {
+      key = "created_at";
+    } else if (text == "lowest_price" || text == "highest_price") {
+      key = "price";
+    }
+    if (order == "desc") {
+      let previousList = productList;
+      let newList = previousList.sort((a: any, b: any) =>
+        a[key] < b[key] ? 1 : b[key] < a[key] ? -1 : 0
+      );
+      setProductList(newList);
+      onPageChange(currentPage);
+    } else {
+      let previousList = productList;
+      let newList = previousList.sort((a: any, b: any) =>
+        a[key] > b[key] ? 1 : b[key] > a[key] ? -1 : 0
+      );
+      setProductList(newList);
+      onPageChange(currentPage);
+    }
+  };
+
   useEffect(() => {
     fetchAllProducts();
   }, [props]);
@@ -148,55 +174,34 @@ export default function Catalog(props: any) {
               : (currentPage + 1) * pageSize}{" "}
             of {productList.length} products
           </span> */}
-          <button className="relative text-sm focus:outline-none group mt-4 sm:mt-0">
-            <div className="flex items-center justify-between w-40 h-10 px-3 border-2 border-gray-300 rounded hover:bg-gray-300">
-              <span className="font-medium">Popular</span>
-              <svg
-                className="w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white shadow-lg rounded group-focus:flex">
-              <a
-                className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                href="#"
-              >
+          <div className="relative text-sm focus:outline-none group mt-4 sm:mt-0">
+            <select
+              name="sortText"
+              id="sortText"
+              className="flex items-center justify-between w-40 h-10 px-3 border-2 bg-white border-gray-300 rounded hover:bg-gray-300"
+              onChange={(e) => {
+                if (e.target.value == "lowest_price") {
+                  sortBy(e.target.value, "asc");
+                } else {
+                  sortBy(e.target.value, "desc");
+                }
+              }}
+            >
+              {/* <option value="blank" key="blank"></option> */}
+              <option value="popular" key="popular">
                 Popular
-              </a>
-              <a
-                className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                href="#"
-              >
-                Featured
-              </a>
-              <a
-                className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                href="#"
-              >
+              </option>
+              <option value="newest" key="newest">
                 Newest
-              </a>
-              <a
-                className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                href="#"
-              >
+              </option>
+              <option value="lowest_price" key="lowest_price">
                 Lowest Price
-              </a>
-              <a
-                className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                href="#"
-              >
+              </option>
+              <option value="highest_price" key="highest_price">
                 Highest Price
-              </a>
-            </div>
-          </button>
+              </option>
+            </select>
+          </div>
           <Filter
             handleFilters={handleFilters}
             isProductFilter={true}
