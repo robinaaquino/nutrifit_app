2cd2793e5064b11c6ad426e64937ec824d6b97b4
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-04-30 02:20:33 +0800

    fix: don't show pagination if no index

--- components/admin/TableComponent.tsx
+++ components/admin/TableComponent.tsx
@@ -248,73 +248,76 @@ export default function TableComponent({
               </div>
             ) : (
               <div className="text-center text-lg font-bold text-black bg-white">
-                No products{" "}
+                No {type}
+                {"s"}
               </div>
             )}
           </div>
         </div>
       </div>
 
-      <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
-        <div className="text-sm text-black ">
-          Page{" "}
-          <span className="font-medium text-black">
-            {currentPage} of {maxPage}
-          </span>
-        </div>
+      {currentProductList.length > 0 ? (
+        <div className="mt-6 sm:flex sm:items-center sm:justify-between ">
+          <div className="text-sm text-black ">
+            Page{" "}
+            <span className="font-medium text-black">
+              {currentPage} of {maxPage}
+            </span>
+          </div>
 
-        <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
-          <button
-            onClick={() => {
-              onPageChange(currentPage - 1);
-            }}
-            disabled={currentPage == 1}
-            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-white capitalize transition-colors duration-200 bg-nf_green border rounded-md sm:w-auto gap-x-2 disabled:bg-gray-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
+          <div className="flex items-center mt-4 gap-x-4 sm:mt-0">
+            <button
+              onClick={() => {
+                onPageChange(currentPage - 1);
+              }}
+              disabled={currentPage == 1}
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-white capitalize transition-colors duration-200 bg-nf_green border rounded-md sm:w-auto gap-x-2 disabled:bg-gray-500"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:-scale-x-100"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                />
+              </svg>
 
-            <span>previous</span>
-          </button>
+              <span>previous</span>
+            </button>
 
-          <button
-            onClick={() => {
-              onPageChange(currentPage + 1);
-            }}
-            disabled={currentPage == maxPage}
-            className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-white capitalize transition-colors duration-200 bg-nf_green border rounded-md sm:w-auto gap-x-2 disabled:bg-gray-500"
-          >
-            <span>Next</span>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
+            <button
+              onClick={() => {
+                onPageChange(currentPage + 1);
+              }}
+              disabled={currentPage == maxPage}
+              className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-white capitalize transition-colors duration-200 bg-nf_green border rounded-md sm:w-auto gap-x-2 disabled:bg-gray-500"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </button>
+              <span>Next</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 rtl:-scale-x-100"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
+      ) : null}
     </>
   );
 }
