aa3e768d70dd191aaca68433fe21adf4c281909b
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-01 16:53:16 +0800

    feat: modified page size to be minimum size of the screen

--- pages/admin/user/index.tsx
+++ pages/admin/user/index.tsx
@@ -83,7 +83,7 @@ export default function AdminOrder(props: any) {
 
   return (
     <>
-      <div className="container px-4 mx-auto">
+      <div className="container px-4 mx-auto min-h-screen">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div>
             <div className="flex items-center gap-x-3">
