731272eb0e9ea3c216db281e6cd937abec7654e6
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-02 15:59:28 +0800

    feat: implemented components for admin analytics

---
+++ components/admin/dashboard/CardStats.tsx
@@ -0,0 +1,120 @@
+import { useRouter } from "next/navigation";
+
+interface CardStatSubValuesTypes {
+  key: string;
+  count: number;
+}
+
+export default function CardStats({
+  mainText,
+  count,
+  subValues,
+  icon,
+}: {
+  mainText: string;
+  count: number;
+  subValues?: CardStatSubValuesTypes[];
+  icon: string;
+}) {
+  const router = useRouter();
+  return (
+    <>
+      <div className="relative flex flex-col min-w-0 break-words bg-nf_green rounded shadow-lg mt-6 mb-6 ml-6 mr-6 w-full">
+        <div
+          className="flex-auto p-4 cursor-pointer hover:bg-nf_dark_blue hover:rounded"
+          onClick={() => {
+            router.push(`/admin/${icon}`);
+          }}
+        >
+          <div className="flex ">
+            <div className="relative w-full pr-4 max-w-full flex-grow flex-1">
+              <h5 className="text-nf_yellow uppercase font-bold text-xs">
+                {mainText}
+              </h5>
+              <span className="font-semibold text-xl text-white">{count}</span>
+            </div>
+            <div className="relative w-auto pl-4 flex-initial">
+              <div
+                className={
+                  "text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-white"
+                }
+              >
+                {icon == "order" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-envelope text-black"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                  </svg>
+                ) : icon == "user" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-people text-black"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z" />
+                  </svg>
+                ) : icon == "product" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-box text-black"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-file text-black"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                  </svg>
+                )}
+              </div>
+            </div>
+          </div>
+          {subValues
+            ? subValues.map((element: any) => {
+                const count = element.count;
+                const key = element.key;
+                return (
+                  <>
+                    <p className="text-sm text-white">
+                      <span className="text-white text-sm ">
+                        {/* <i
+                        className={
+                          "fas fa-arrow-up"
+                          // statArrow === "up"
+                          //   ? "fas fa-arrow-up"
+                          //   : statArrow === "down"
+                          //   ? "fas fa-arrow-down"
+                          //   : ""
+                        }
+                      ></i>{" "} */}
+                        {count}{" "}
+                      </span>
+                      <span className="whitespace-nowrap">{key}</span>
+                    </p>
+                  </>
+                );
+              })
+            : null}
+        </div>
+      </div>
+    </>
+  );
+}
