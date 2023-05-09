7467e5c0f381f399867a752aef4cd99d1adfcb87
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-09 20:54:29 +0800

    style: fixed price going off the modal

--- components/universal/filter.tsx
+++ components/universal/filter.tsx
@@ -166,34 +166,37 @@ export default function Filter({
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full justify-center p-4 text-center items-center ">
               <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-filter text-black"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
-                      </svg>
-                    </div>
-                    <div className="mt-3 text-left ml-2">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 max-w-full min-w-full">
+                  <div className="">
+                    <div className="flex items-center ">
+                      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-red-100 ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-filter text-black"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                      </div>
                       <h3
-                        className="text-base font-semibold leading-6 text-gray-900"
+                        className="ml-3 font-semibold leading-6 text-gray-900"
                         id="modal-title"
                       >
                         Filters
                       </h3>
+                    </div>
+
+                    <div className="mt-3 text-left ml-2">
                       {/* <form onSubmit={(e) => e.preventDefault()}> */}
-                      <div className="m-2 w-auto h-auto">
+                      <div className="m-2 w-full h-auto">
                         {isProductFilter ? (
                           <>
-                            <div>
+                            <div className="">
                               {/* Category Filter */}
                               <div className="p-2">
                                 <div className="flex text-black">
@@ -229,7 +232,7 @@ export default function Filter({
                                 <div className="flex pt-2">
                                   <input
                                     type="number"
-                                    className=" w-auto bg-white text-black "
+                                    className="w-full bg-white text-black "
                                     placeholder="Minimum price..."
                                     onChange={(event) =>
                                       setFilterMinPrice(
@@ -240,7 +243,7 @@ export default function Filter({
                                   <p className="text-black px-2 text-lg">-</p>
                                   <input
                                     type="number"
-                                    className=" w-auto bg-white text-black "
+                                    className=" w-full bg-white text-black "
                                     placeholder="Maximum price..."
                                     onChange={(event) =>
                                       setFilterMaxPrice(
@@ -293,7 +296,7 @@ export default function Filter({
                                 <div className="flex pt-2">
                                   <input
                                     type="number"
-                                    className=" w-auto bg-white text-black "
+                                    className="w-full bg-white text-black "
                                     placeholder="Minimum price..."
                                     onChange={(event) =>
                                       setFilterMinPrice(
@@ -304,7 +307,7 @@ export default function Filter({
                                   <p className="text-black px-2 text-lg">-</p>
                                   <input
                                     type="number"
-                                    className=" w-auto bg-white text-black "
+                                    className=" w-full bg-white text-black "
                                     placeholder="Maximum price..."
                                     onChange={(event) =>
                                       setFilterMaxPrice(
@@ -410,7 +413,7 @@ export default function Filter({
                           </>
                         ) : null}
                       </div>
-                      <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <div className=" py-3 flex flex-row-reverse px-6">
                         <button
                           type="submit"
                           className="inline-flex w-full justify-center rounded-md bg-nf_green px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-nf_dark_blue sm:ml-3 sm:w-auto"
