6c414314c24e622e0efa842277e7d143d0d99e48
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-14 23:35:58 +0800

    style: different styles for submit buttons

--- components/forms/input/InputSubmit.tsx
+++ components/forms/input/InputSubmit.tsx
@@ -1,11 +1,20 @@
-export default function InputSubmit({ label }: { label: any }) {
+export default function InputSubmit({
+  label,
+  type,
+}: {
+  label: any;
+  type?: any;
+}) {
   return (
     <>
       <input
         type="submit"
         value={label}
-        // className="border-primary w-full cursor-pointer rounded-md border bg-nf_green py-3 px-5 text-base text-white transition hover:bg-nf_dark_green mb-6"
-        className="inline-flex w-full justify-center rounded-md bg-nf_green px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-nf_dark_blue cursor-pointer sm:ml-3 sm:w-auto"
+        className={
+          type == "multiple"
+            ? `inline-flex w-full justify-center rounded-md bg-nf_green px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-nf_dark_blue cursor-pointer sm:ml-3 sm:w-auto`
+            : `w-full font-semibold cursor-pointer rounded-md border bg-nf_green py-3 px-5 text-base text-white transition hover:bg-nf_dark_blue mb-6`
+        }
       />
     </>
   );
