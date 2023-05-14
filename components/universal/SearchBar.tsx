1bdea222cf44f0068abccbd7ce891582c8827c93
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-15 03:09:32 +0800

    style: put search bar on the middle
    refactor: changed search bar component file name
    chore: removed comments

---
+++ components/universal/SearchBar.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+
+export default function SearchBar({ handleSearch }: { handleSearch: any }) {
+  const [searchString, setSearchString] = useState<string>("");
+  return (
+    <>
+      <div className="pt-2 relative mx-auto text-gray-600">
+        <input
+          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none focus:text-black"
+          type="search"
+          name="search"
+          placeholder="Search for a product..."
+          onChange={(e) => {
+            setSearchString(e.target.value);
+          }}
+          onKeyUp={(event) => {
+            if (event.code === "Enter") handleSearch(searchString);
+          }}
+        />
+        <button
+          type="submit"
+          className="absolute right-0 top-0 mt-5 mr-4"
+          onClick={() => handleSearch(searchString)}
+        >
+          <svg
+            className="text-gray-600 h-4 w-4 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            id="Capa_1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 56.966 56.966"
+            width="512px"
+            height="512px"
+          >
+            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+}
+
+{
+  /* <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."></input> */
+}
