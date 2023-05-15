aa066d7851d69462dbd7d7eefe555506a113b055
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-15 15:18:00 +0800

    feat: added id to HeadingTwo

--- components/forms/HeadingTwo.tsx
+++ components/forms/HeadingTwo.tsx
@@ -1,7 +1,7 @@
-export default function HeadingTwo({ label }: { label: any }) {
+export default function HeadingTwo({ label, id }: { label: any; id?: any }) {
   return (
     <>
-      <h2 className="mb-4 font-bold md:text-xl text-heading text-black">
+      <h2 className="mb-4 font-bold md:text-xl text-heading text-black" id={id}>
         {label}
       </h2>
     </>
