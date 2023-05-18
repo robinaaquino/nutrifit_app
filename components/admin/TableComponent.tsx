54ab71cab675ad5ec067350405b4b686360d5f0d
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-05-19 06:41:31 +0800

    feat: hide delete if order table

--- components/admin/TableComponent.tsx
+++ components/admin/TableComponent.tsx
@@ -331,7 +331,7 @@ export default function TableComponent({
                                   >
                                     Edit
                                   </button>
-                                  {type == "user" ? null : (
+                                  {type == "user" || type == "order" ? null : (
                                     <button
                                       className="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-red-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-nf_dark_blue ml-2"
                                       onClick={(e: any) => {
